cb2a1769383f6c9d459ca9b5c33319726f150a19
Shaikat0079 <baruashaikat00790@gmail.com>
2025-04-04 03:42:43 +0930

    Finally I've done it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+ const [score,setScore] = useState(0)
 
+ const handleScore = ()=>{
+  const run = score+1;
+  setScore(run)
+ }
+ const addFive = ()=>{
+  const run = score+5;
+  setScore(run)
+ }
   return (
     <>
-     
+     <h1>Score: {score}</h1>
+     <button onClick={handleScore}>Add Score</button>
+     <button onClick={addFive}>Add Five</button>
     </>
   )
 }
